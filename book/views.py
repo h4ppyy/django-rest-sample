753a4c178075234f3fc0972dc6ea869db5c53bfc
h4ppyy <93immm@naver.com>
2020-11-03 17:11:27 +0900

    remove class

--- book/views.py
+++ book/views.py
@@ -5,10 +5,6 @@ from django.db import connections
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
 
-from rest_framework.views import APIView
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import action
 from rest_framework.decorators import api_view
 
 from drf_yasg import openapi
