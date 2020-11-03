552f817148c00b3b0e4a933ce2b03fef6a41f43e
h4ppyy <93immm@naver.com>
2020-11-03 17:10:35 +0900

    function programming

--- book/urls.py
+++ book/urls.py
@@ -3,10 +3,16 @@ from django.conf.urls import url
 
 from rest_framework.routers import DefaultRouter
 
-from book.views import BooksViewSet
+# from book.views import BooksViewSet
 
+from book import views as bookViews
 
-router = DefaultRouter()
-router.register(r'books', BooksViewSet, basename='books')
+# router = DefaultRouter()
+# router.register(r'books', BooksViewSet, basename='books')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+
+urlpatterns = [
+    path('book/list', bookViews.book_list, name='book_list'),
+    path('book/create', bookViews.book_create, name='book_create'),
+] 
--- book/views.py
+++ book/views.py
@@ -1,17 +1,54 @@
 import json
 import datetime
+
 from django.db import connections
 from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 
 from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.decorators import api_view
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 from book.models import *
 from book.serializers import BookSerializer
 
 
+@api_view(['GET'])
+def book_list(request):
+    # query
+    books = Book.objects.all()
+    
+    # serialize
+    serializer = BookSerializer(books, many=True)
+    book_list = serializer.data
+
+    return JsonResponse({'result_code': 200, 'result_data': book_list})
+
+
+@swagger_auto_schema(
+    methods=['post'], 
+    request_body=BookSerializer, 
+    responses={
+        200: openapi.Response('hello world', BookSerializer)
+    }
+)
+@api_view(['POST'])
+def book_create(request):
+    # parameter
+    book_name = request.data['book_name']
+
+    # query
+    Book(book_name=book_name, created_at=datetime.datetime.now()).save()
+
+    return JsonResponse({'result': 200})
+
+
+'''
 class BooksViewSet(viewsets.ViewSet):
 
     serializer_class = BookSerializer
@@ -36,6 +73,16 @@ class BooksViewSet(viewsets.ViewSet):
 
         return Response({'result_code': 200, 'result_data': book_data})
 
+    # test_param = openapi.Parameter(
+    #     'test', 
+    #     openapi.IN_BODY, 
+    #     description="test manual param", 
+    #     type=openapi.TYPE_OBJECT
+    # )
+    # user_response = openapi.Response('response description', BookSerializer)
+    # @swagger_auto_schema(method='post', manual_parameters=[test_param], responses={200: user_response})
+    @swagger_auto_schema(methods=['post'], request_body=BookSerializer)
+    @api_view(['POST'])
     def create(self, request, pk=None):
         # parameter
         book_name = request.data['book_name']
@@ -62,3 +109,4 @@ class BooksViewSet(viewsets.ViewSet):
         book.delete()
 
         return Response({'result': 200})
+'''
